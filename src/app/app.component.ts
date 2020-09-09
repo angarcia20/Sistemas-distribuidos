90cd4e93cab8549c19c3c75b6d896336ae4347ee
Jimmy Giovanny Forero Forero <jiforero2@poligran.edu.co>
2020-09-08 20:06:00 -0500

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ClienteService } from './services/cliente.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'estudiantes';
+  estudiantes:any = [];
+
+  constructor(private clienteService: ClienteService){
+    this.estudiantes = this.clienteService.getAllEstudents(). subscribe(
+      estudent => {
+        this.estudiantes.push(estudent);
+      }, error => console.error(error)
+    )
+  }
+  
 }
